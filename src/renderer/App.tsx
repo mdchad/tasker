f50063951fa8a9c7cb1f4d134a0bcf21f445ece8
chad <irsyad.muhd@gmail.com>
2019-02-10 13:39:54 +0800

    tray icon works

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,50 +4,84 @@
 import '@public/style.css';
 // tslint:disable-next-line
 import { remote } from 'electron';
-const { Menu, MenuItem } = remote
+const { Tray, Menu, MenuItem , systemPreferences } = remote
+import * as path from 'path';
 import * as React from 'react';
 
 class App extends React.Component<{}, {}> {
-    private menu: any= new Menu()
+    public state: { toggle: boolean } = {
+        toggle: false
+    };
+    private menu: any = new Menu();
 
     public componentDidMount(): any {
+        const a: any = this.getTray;
         this.menu.append(new MenuItem ({
-            label: 'MenuItem1',
+            label: 'Open tray',
             click(): any {
-                console.log('item 1 clicked')
+                console.log('item 1 clicked');
+                // @ts-ignore
+                a();
             }
-        }))
+        }));
     }
+
+
+    public getTray = (): any => {
+        // tslint:disable-next-line
+        let self = this;
+        const trayIcon: any = systemPreferences.isDarkMode()
+            ? new Tray(path.join('',
+                '/Users/chad/' +
+                'Desktop/code/tasker/dist/assets/icons/png/16x16-white.png'))
+            : new Tray(path.join('',
+                '/Users/chad/' +
+                'Desktop/code/tasker/dist/assets/icons/png/16x16-white.png'));
+
+        const trayMenuTemplate: any = [
+            {
+                label: 'Empty Application',
+                enabled: false
+            },
+
+            {
+                label: 'Settings',
+                click(): any {
+                    console.log('Clicked on settings');
+                }
+            },
+
+            {
+                label: 'Help',
+                click(): any {
+                    self.setState({ toggle: true });
+                    console.log('Clicked on Help');
+                }
+            }
+        ];
+
+        const trayMenu: any = Menu.buildFromTemplate(trayMenuTemplate);
+        // this.trayIcon.setHighlightMode('always');
+        trayIcon.getBounds();
+        trayIcon.setContextMenu(trayMenu);
+        trayIcon.setToolTip('this is an app');
+    }
+
     //
     public getClick = (e: any): any => {
         e.preventDefault()
         // @ts-ignore
         this.menu.popup(remote.getCurrentWindow());
-        // console.log('hello world', Menu);
-        // const template: any = [
-        //     {
-        //         label: 'Filter',
-        //         submenu: [
-        //             {
-        //                 label: 'Hello',
-        //                 accelerator: 'Shift+CmdOrCtrl+H',
-        //                 click(): any {
-        //                     // tslint:disable-next-line
-        //                     console.log('hello world')
-        //                 }
-        //             }
-        //         ]
-        //     }
-        // ];
-        // const menu: Menu = Menu.buildFromTemplate(template);
-        // menu.popup({});
     }
 
     // tslint:disable-next-line
     render() {
         return (
             <div className='app' onContextMenu={(e) => this.getClick(e)}>
-                <h4>Welcome to React, Electron and Typescript hello world</h4>
+                <h3 className='title'>Tasker</h3>
+                <input type='text'
+                       placeholder='What are you doing today'
+                       className='input-class'/>
             </div>
         );
     }
